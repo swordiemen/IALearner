5688e056c83ce9c5871b80b1b00a4c819cc16fff
swordiemen <tim.blokk@gmail.com>
2016-01-05 16:02:54 +0100

    Formula altered

--- IALearner/src/classifier/Classifier.java
+++ IALearner/src/classifier/Classifier.java
@@ -9,6 +9,7 @@ public class Classifier {
 	private HashMap<String, List<ClassDictionary>> categories;
 	private String currentCategorie;
 	private Tokenizer tokenizer;
+	private String punten = "."; //can be 1 dot or 2 dots depending on your system.
 
 	public Classifier() {
 		tokenizer = new Tokenizer();
@@ -46,12 +47,13 @@ public class Classifier {
 	}
 
 	public String classify(File f) {
-		double maxProbe = -1;
+		double maxProbe = Double.NEGATIVE_INFINITY;
 		String maxClass = "";
 		for (ClassDictionary dictionary : categories.get(currentCategorie)) {
-			System.out.println(dictionary.probSentence(tokenizer.getTokens(f)));
-			if (dictionary.probSentence(tokenizer.getTokens(f)) > maxProbe) {
-				maxProbe = dictionary.probSentence(tokenizer.getTokens(f));
+			double prob = dictionary.probSentence(tokenizer.getTokens(f));
+			System.out.println("ProbSentence for " + dictionary.getClassName() + ": " + prob);
+			if (prob > maxProbe) {
+				maxProbe = prob;
 				maxClass = dictionary.getClassName();
 			}
 		}
@@ -59,34 +61,47 @@ public class Classifier {
 	}
 
 	public static void main(String[] args) {
+		int succeeds = 0;
+		int f = 0;
+		String punten = "."; //can be 1 dot or 2 dots depending on your system.
 		Classifier c = new Classifier();
 		c.createCategorie("blogs", new String[] { "F", "M" });
 		c.selectCurrentClass("blogs");
 		for (int i = 1; i < 600; i++) {
-			if(new File("./blogs/F/F-train" + i
+			if(new File(punten + "/blogs/F/F-train" + i
 					+ ".txt").exists()){
-			c.train(new File("./blogs/F/F-train" + i + ".txt"), "F");
+			c.train(new File(punten + "/blogs/F/F-train" + i + ".txt"), "F");
 			}else{
-			c.train(new File("./blogs/M/M-train" + i + ".txt"), "M");
+			c.train(new File(punten + "/blogs/M/M-train" + i + ".txt"), "M");
 			}
 		}
 		System.out.println(c.getClass("F").getTotal());
 		System.out.println(c.getClass("M").getTotal());
 		for (int i = 1; i < 50; i++) {
 			System.out.print("Testing test" + i);
-			if(new File("./blogs/F/F-test" + i
+			if(new File(punten + "/blogs/F/F-test" + i
 					+ ".txt").exists()){
 				System.out.print(" (F)\n");
-				System.out.println((c.classify(new File("./blogs/F/F-test" + i
-						+ ".txt"))).equals("F"));
+				boolean succeeded = (c.classify(new File(punten + "/blogs/F/F-test" + i
+						+ ".txt"))).equals("F");
+				if(succeeded){
+					succeeds++;
+					f++;
+				}
+				System.out.println(succeeded);
 			}else{
 				System.out.print(" (M)\n");
-				System.out.println(c.classify(
-						new File("./blogs/M/M-test" + i + ".txt"))
-						.equals("M"));
+				boolean succeeded = c.classify(
+						new File(punten + "/blogs/M/M-test" + i + ".txt"))
+						.equals("M");
+				if(succeeded){
+					succeeds++;
+				}
+				System.out.println(succeeded);
 			}
 		}
-		System.out.println(c.classify(new File("./blogs/F/F-test2.txt")));
+		//System.out.println(c.classify(new File(punten + "/blogs/F/F-test2.txt")));
+		System.out.println("Succeeds: " + succeeds + " of 49, of which " + f + " were f");
 	}
 
 }
